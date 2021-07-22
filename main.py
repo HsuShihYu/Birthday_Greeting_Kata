ee01c51915bdde2a35e759a438c0a8569e25f53f
HsuShihYu <sam8809210@gmail.com>
2021-07-23 05:16:57 +0800

    Version 5: Simple Message but different output data format

--- main.py
+++ main.py
@@ -30,59 +30,38 @@ now_date = str(int(now_month)) + "/" + str(int(now_day))
 print("Today is " + now_date)
 now_year = int(str(datetime.datetime.now())[:4])
 
-#Version 4: Simple Message with full name 
-@app.route("/api/v4/simple_messege_fullname", methods=['POST'])
-def simple_messege_fullname():
-    sql = "SELECT * FROM MEMBER WHERE Date_of_Birth LIKE '%" + now_date + "%\'"
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+#Version 5: Simple Message but different output data format 
+@app.route("/api/v5/simple_messege_xml", methods=['POST'])
+def simple_messege_xml():
+    try:
+        sql = "SELECT * FROM MEMBER WHERE Date_of_Birth LIKE '%" + now_date + "%\'"
+        mycursor = mydb.cursor()
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall()
 
-    data = {}
-    for row in myresult:
-        email = str(row[5])
-        message = {}
-        message["title"] = 'Subject: Happy birthday!'
-        message["content"] = 'Happy birthday, dear ' + str(row[1]) + ", " + str(row[2]) + '!'
-        msg = Message(
-            subject="Happy birthday!",
-            recipients=[email],
-            html=json.dumps(message)
-        )
-        data[row[0]] = message
-        mail.send(msg)
+        data = {}
+        for row in myresult:
+            email = str(row[5])
 
-    res = json.dumps(data)
-    return res
-
-#Version 4: Simple Message but database changes 
-@app.route("/api/v4/simple_messege_mongodb", methods=['POST'])
-def simple_messege_mongodb(): 
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["mydatabase"]
-    mycol = mydb["MEMBER"]
-    myquery = { "Date_of_Birth": { "$regex":now_date} }
-    mydoc = mycol.find(myquery)
-
-    data = {}
-    index = 0
-    for row in mydoc:
-        email = str(row['Email'])
-        message = {}
-        message["title"] = 'Subject: Happy birthday!'
-        message["content"] = 'Happy birthday, dear ' + str(row['First_Name']) + '!'
-
-        msg = Message(
-            subject='Subject: Happy birthday!',
-            recipients=[email],
-            html= json.dumps(message)
-        )
-        data[index] = message
-        index = index + 1
-        mail.send(msg)
+            message = {}
+            message["tittle"] = 'Subject: Happy birthday! '
+            message["content"] = 'Happy birthday, dear ' + str(row[1]) + '!'
+            msg = Message(
+                subject= "Happy birthday!",
+                recipients=[email],
+                html= dict2xml(message)
+            )
+            data[row[0]] = message
+            mail.send(msg)
         
-    res = json.dumps(data)
-    return res
+        res = dict2xml(data)
+        return res
+    
+    except Exception as e:
+        print(e)
+        res2 = jsonify("Fail")
+        res2.status_code = 400
+        return res2
 
 
 if __name__ == '__main__':
